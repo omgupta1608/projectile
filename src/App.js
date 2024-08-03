c88acf72494d75dfb783f28dcc75c3e0b0593152
Om Gupta <omgupta1608@gmail.com>
2024-08-03 19:45:01 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MovingCircle from './MovingCircle';
+
+function App() {
+  return (
+    <div className="App">
+      <MovingCircle></MovingCircle>
+    </div>
+  );
+}
+
+export default App;
